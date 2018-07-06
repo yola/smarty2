3337909766bd9b2e08637e7c4c1c11446f2c2a77
Stefano Rivera <stefano@yola.com>
2018-07-06 08:43:43 -0700

    each() is deprecated in PHP 7.2

--- libs/Smarty_Compiler.class.php
+++ libs/Smarty_Compiler.class.php
@@ -559,7 +559,8 @@ class Smarty_Compiler extends Smarty {
 
             case 'php':
                 /* handle folded tags replaced by {php} */
-                list(, $block) = each($this->_folded_blocks);
+                $block = current($this->_folded_blocks);
+                next($this->_folded_blocks);
                 $this->_current_line_no += substr_count($block[0], "\n");
                 /* the number of matched elements in the regexp in _compile_file()
                    determins the type of folded tag that was found */
